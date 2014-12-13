b3f7f94eaa176afa24aea93cc6bb985bdfbc18c8
sven <sven@email.com>
2014-12-13 13:10:21 +0100

    nokay

--- src/Domain/Component.java
+++ src/Domain/Component.java
@@ -1,5 +1,17 @@
 package Domain;
 
-public class Component {
+import java.util.ArrayList;
 
+public abstract class Component {
+private ArrayList<Component> allPatterns = new ArrayList<Component>();
+	
+	public abstract String getName();
+	
+	
+	
+	
+	
+	public void addPattern(Component c1){
+		allPatterns.add(c1);
+	}
 }
--- src/Domain/Pattern.java
+++ src/Domain/Pattern.java
@@ -1,5 +1,20 @@
 package Domain;
 
-public class Pattern {
+import java.awt.Image;
 
+public class Pattern extends Component{
+	private String name, context, problem, solution, consequences, imageLink;
+	private Image image;
+	
+	public Pattern(String nm, String cont, String pr, String cons, String il){
+		name = nm;
+		context = cont;
+		problem = pr;
+		consequences = cons;
+		imageLink = il;
+	}
+	
+	public String getName(){
+		return name;
+	}
 }
