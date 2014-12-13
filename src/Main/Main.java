3b79f1f5d520e7c4c7ce690cf06142318d271cf0
Sjuuls <julien_noel@msn.com>
2014-12-13 14:19:58 +0100

    EditPatternGUI 13/12/2014 14:19

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,23 @@
 package Main;
 
+import java.awt.EventQueue;
+
+import View.EditPatternGUI;
+
 public class Main {
-	public static void main(String[] arg){
-		
+	/**
+	 * Launch the application EditPatternGUI.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EditPatternGUI window = new EditPatternGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
