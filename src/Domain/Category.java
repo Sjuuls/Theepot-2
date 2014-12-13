59950edb0cdae195a6bde27425973f5ee09f32a8
sven <sven@email.com>
2014-12-13 12:54:04 +0100

    jeej

--- src/Domain/Category.java
+++ src/Domain/Category.java
@@ -1,5 +1,16 @@
 package Domain;
 
-public class Category {
+import java.util.ArrayList;
 
+public class Category extends Component {
+	private ArrayList<Component> allComponent = new ArrayList<Component>();
+	private String name;
+
+	public Category(String nm){
+		name = nm;
+	}
+	
+	public String getName(){
+		return name;
+	}
 }
